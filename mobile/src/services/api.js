b0d74084ac6465d446d7f9f60fe4e36bbd0fb9be
LinderJunior <linderju01@gmail.com>
2022-06-15 14:09:14 +0100

    configurando o front da app

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://192.168.43.110:3333'
-  baseURL: 'http://192.168.8.118:3333'
+ // baseURL: 'http://192.168.8.118:3333'
+    baseURL: 'http://10.20.1.145:3000'
 });
 
 export default api;
